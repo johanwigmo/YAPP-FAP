611e5fa84ff2b84228eeb53c1917b3086fbbd7e5
Johan Wigmo <johanwigmo@gmail.com>
2019-01-17 10:14:58 +0100

    Initial WeatherApp commit

---
+++ 17 Fetch Data/WeahterApp/WeahterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeahterApp
+//
+//  Created by Johan Wigmo on 2019-01-17.
+//  Copyright © 2019 Wigmo Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
