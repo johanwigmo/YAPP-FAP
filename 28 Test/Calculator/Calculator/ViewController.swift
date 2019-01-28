5e553ea44110e3d1d08e762564f151c1d336a8c1
Johan Wigmo <johanwigmo@gmail.com>
2019-01-28 11:23:23 +0100

    Initial calculator commit

---
+++ 28 Test/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Johan Wigmo on 2019-01-28.
+//  Copyright © 2019 Newton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
